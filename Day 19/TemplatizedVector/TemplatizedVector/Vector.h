6065c3e5d7483c1f5f65351df0a00432a7188107
DylanWeiner <adhdylanaza@gmail.com>
2025-09-11 13:18:02 -0600

    Completed Templatized Vector.

--- Day 19/TemplatizedVector/TemplatizedVector/Vector.h
+++ Day 19/TemplatizedVector/TemplatizedVector/Vector.h
@@ -12,13 +12,18 @@
 
 #include <cassert>
 
+#include <string>
+
+template <typename T>
 class vector {
 private:
-    int * array;
+    T * array;
     int capacity; //absolute max.
     int size; //active ints being stored
     
 public:
+    vector();
+    
     vector(int capacity);
     
     void operator=(const vector & rhs);
@@ -34,18 +39,176 @@ public:
     int viewSize();
     
     vector makeVector(int initialCapacity);
-
+    
     void freeVector();
-
-    void pushBack(int newVal);
-
+    
+    void pushBack(T newVal);
+    
     void popBack();
-
+    
     int get(int index);
-
-    void set(int index, int newValue);
-
+    
+    void set(int index, T newValue);
+    
     void growVector();
+    
+    bool operator==(const vector & rhs);
+    
+    bool operator!=(const vector & rhs);
+    
+    bool operator<(const vector & rhs);
+    
+    bool operator<=(const vector & rhs);
+    
+    bool operator>(const vector & rhs);
+    
+    bool operator>=(const vector & rhs);
 };
+
+template <typename T>
+vector<T>::vector() {
+    array = new T[0];
+    capacity = 1;
+    size = 0;
+}
+
+template <typename T>
+vector<T>::vector(int initialCapacity) {
+    array = new T[initialCapacity];
+    capacity = initialCapacity;
+    size = 0;
+}
+
+template <typename T>
+void vector<T>::operator=(const vector & rhs) {
+    (*this).array = rhs.array;
+    (*this).capacity = rhs.capacity;
+    (*this).size = rhs.size;
+}
+
+template <typename T>
+vector<T>::vector(const vector& rhs) {
+    (*this).array = new int[rhs.capacity];
+    (*this).capacity = rhs.capacity;
+    (*this).size = rhs.size;
+    
+    for(int i = 0; i < size; i++) {
+        this->array[i] = rhs.array[i];
+    }
+}
+
+template <typename T>
+int& vector<T>::operator[](const int index) {
+    return *(array + index);
+} // This redefines a value at an index.
+
+template <typename T>
+int vector<T>::operator[](const int index)const {
+    return *(array + index);
+} // This reads a value in an array so it can be output.
+
+template <typename T>
+int vector<T>::viewCapacity() {
+    return capacity;
+}
+
+template <typename T>
+int vector<T>::viewSize() {
+    return size;
+}
+
+template <typename T>
+void vector<T>::freeVector() {
+    delete[] array;
+}
+
+template <typename T>
+void vector<T>::pushBack(T newVal) {
+    *(array + size) = newVal;
+    size++;
+}
+
+template <typename T>
+void vector<T>::popBack(){
+    array[size] = NULL;
+    size--;
+}
+
+template <typename T>
+int vector<T>::get(int index) {
+    return *(array + index);
+}
+
+template <typename T>
+void vector<T>::set(int index, T newValue) {
+    *(array + index) = newValue;
+}
+
+template <typename T>
+void vector<T>::growVector() {
+    capacity *= 2;
+    int* newCap = new int(capacity);
+    for(int i = 0; i < size; i++) {
+        *(newCap + i) = *(array + i);
+    }
+    freeVector();
+    array = newCap;
+}
+
+template <typename T>
+bool vector<T>::operator==(const vector & rhs) {
+    if((*this).size != rhs.size) {
+        return false;
+        for (int i = 0; i < (*this).size; i++) {
+            if((*this).array[i] != rhs.array[i])
+                return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+bool vector<T>::operator!=(const vector & rhs) {
+    if((*this)==rhs) {
+        return false;
+    }
+    return true;
+}
+
+template <typename T>
+bool vector<T>::operator<(const vector & rhs) {
+    if((*this).size > rhs.size) {
+        return false;
+        for(int i = 0; i < rhs.size; i++) {
+            if((*this).array[i] > rhs.array[i])
+                return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+bool vector<T>::operator<=(const vector & rhs) {
+    if((*this) < rhs || (*this) == rhs) {
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+bool vector<T>::operator>(const vector & rhs) {
+    if((*this) < rhs || (*this) == rhs) {
+        return false;
+    }
+    return true;
+}
+
+template <typename T>
+bool vector<T>::operator>=(const vector & rhs) {
+    if((*this) < rhs) {
+        return false;
+    }
+    return true;
+}
 
 #endif
--- Day 19/TemplatizedVector/TemplatizedVector/main.cpp
+++ Day 19/TemplatizedVector/TemplatizedVector/main.cpp
@@ -7,8 +7,21 @@
 
 #include "Vector.h"
 
+
 int main(int argc, const char * argv[]) {
-    vector vec (20);
+    vector<int> vec (20);
+    
+    vector<std::string> vec2 (20);
+    
+    vector<int> vec3 (20);
+    
+    vector<int> vec4 (20);
+    
+    vector<int> vec5 (19);
+    
+    vector<std::string> vec6 (20);
+    
+    vector<std::string> vec7 (20);
     
     assert(vec.viewCapacity()==20);
     
@@ -18,6 +31,49 @@ int main(int argc, const char * argv[]) {
     vec.pushBack(14);
     vec.pushBack(6);
     vec.pushBack(9);
+    
+    vec3.pushBack(10);
+    vec3.pushBack(28);
+    vec3.pushBack(93);
+    vec3.pushBack(14);
+    vec3.pushBack(6);
+    vec3.pushBack(9);
+    
+    vec2.pushBack("Egg ");
+    vec2.pushBack("Salad ");
+    vec2.pushBack("Sandwich ");
+    vec2.pushBack("Yum!");
+    
+    vec4.pushBack(10);
+    vec4.pushBack(28);
+    vec4.pushBack(93);
+    vec4.pushBack(14);
+    vec4.pushBack(6);
+    vec4.pushBack(9);
+    
+    vec5.pushBack(10);
+    vec5.pushBack(28);
+    vec5.pushBack(93);
+    vec5.pushBack(14);
+    vec5.pushBack(6);
+    
+    vec6.pushBack("attack");
+    vec6.pushBack("on");
+    vec6.pushBack("titan");
+    
+    vec7.pushBack("full");
+    vec7.pushBack("metal");
+    vec7.pushBack("alchemist");
+    
+    assert(vec4 >= vec5);
+    
+    assert(vec5 <= vec4);
+    
+    assert(vec5 != vec4);
+    
+    assert(vec == vec3);
+    
+    assert(vec7 < vec6);
     
     assert(vec.get(0)==10);
     
@@ -36,6 +92,8 @@ int main(int argc, const char * argv[]) {
     vec.set(1, 99);
     
     assert(vec.get(1) == 99);
+    
+
     
     return 0;
 }
