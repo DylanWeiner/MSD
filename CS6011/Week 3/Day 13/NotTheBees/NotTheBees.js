cd85bf201c41856da56a53e7f4596c47a8981c4d
DylanWeiner <adhdylanaza@gmail.com>
2025-10-22 12:34:30 -0600

    Finished Not the Bees

--- CS6011/Week 3/Day 13/NotTheBees/NotTheBees.js
+++ CS6011/Week 3/Day 13/NotTheBees/NotTheBees.js
@@ -1,21 +1,31 @@
 let beeimg = new Image(); // Alternatively document.createElement('img');
-beeimg.src = "bee-left1.png"; // This should be the file name of the bee image.
+beeimg.src = "bee.png"; // This should be the file name of the bee image.
 
-let bee = {x: 200, y: 100, speed: Math.random()*5+5};
-bee.img = beeimg;
+let bee1 = {x: 15, y: 25, speed: Math.random()*5+5};
+bee1.img = beeimg;
 
-// let bee2 = {x: 220, y: 110, speed: Math.random()*5+5};
-// bee2.img = beeimg;
+let bee2 = {x: 250, y: 600, speed: Math.random()*5+5};
+bee2.img = beeimg;
 
-// let bee3 = {x: 180, y: 85, speed: Math.random()*5+5};
-// bee3.img = beeimg;
+let bee3 = {x: 150, y: 425, speed: Math.random()*5+5};
+bee3.img = beeimg;
+
+let bee4 = {x: 630, y: 120, speed: Math.random()*5+5};
+bee4.img = beeimg;
+
+let bee5 = {x: 510, y: 20, speed: Math.random()*5+5};
+bee5.img = beeimg;
+
+let bees = [bee1, bee2, bee3, bee4, bee5]; // 1024 x 1024
 
 let myCanvas = document.querySelector("canvas");
 myCanvas.style.border = "2px solid green";
 let ctx = myCanvas.getContext('2d');
 
-let mouseX = bee.x;
-let mouseY = bee.y;
+let mouseX = 0;
+let mouseY = 0;
+let score = 0;
+let collision = false;
 
 function recordMousePos(event) {
   const rect = myCanvas.getBoundingClientRect(); // Finds the canvas's size and correctly adjusts coordinates
@@ -26,40 +36,129 @@ function recordMousePos(event) {
 
 // Causes the bees to follow the mouse
 function updateBees() {
-    let dx = mouseX - bee.x;
-	if(Math.abs(dx) < bee.speed){
-		bee.x += dx;
-	}
-	else if(bee.x < mouseX) {
-		bee.x += bee.speed;
-	}
-	else {
-		bee.x -= bee.speed;
-	}
+   for(let i =0; i < bees.length; i++){
+        let dx = mouseX - bees[i].x;
+        if(Math.abs(dx) < bees[i].speed){
+            bees[i].x += dx;
+        }
+        else if(bees[i].x < mouseX) {
+            bees[i].x += bees[i].speed;
+        }
+        else {
+            bees[i].x -= bees[i].speed;
+        }
 
-    let dy = mouseY - bee.y;
-	if(Math.abs(dy) < bee.speed){
-		bee.y += dy;
-	}
-	else if(bee.y < mouseY) {
-		bee.y += bee.speed;
-	}
-	else {
-		bee.y -= bee.speed;
-	}
+        let dy = mouseY - bees[i].y;
+        if(Math.abs(dy) < bees[i].speed){
+            bees[i].y += dy;
+        }
+        else if(bees[i].y < mouseY) {
+            bees[i].y += bees[i].speed;
+        }
+        else {
+            bees[i].y -= bees[i].speed;
+        }
+    }
 }
 
-// Render the bee and request next frame
 function draw() {
   ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  ctx.drawImage(beeimg, bee.x, bee.y, 50, 50);
-  updateBees();
-  requestAnimationFrame(draw);
+  for(let bee of bees){
+    ctx.drawImage(bee.img, bee.x, bee.y, 50, 50);
+  }
+  if(isAnimating){
+    // updateBees();
+    collisionDetected();
+    animationId = requestAnimationFrame(draw);
+
+    if(!collision) {
+      setTimeout(() => {
+        score += 10;
+      }, 100);
+     }
+
+    if(collision) {
+      alert("Game Over: You lose!\n Your Score was " + score);
+      score = 0;
+    }
+  }
+}
+
+function beeStop() {
+  isAnimating = false;
+  if(animationId){
+    cancelAnimationFrame(animationId);
+  }
+}
+
+function beeStart() {
+  if(!isAnimating){
+    isAnimating = true;
+    draw();
+  }
+
+  // Hide the default cursor on the whole page
+  document.body.style.cursor = 'none';
+
+  // Create an image element dynamically
+  const customCursor = document.createElement('img');
+  customCursor.src = 'honey.png'; // 2048 x 2048
+  customCursor.style.position = 'absolute';
+  customCursor.style.width = '40px';   // customize size
+  customCursor.style.height = '40px';
+  customCursor.style.pointerEvents = 'none'; // so it doesn't block mouse events
+  customCursor.style.zIndex = '9999';
+  customCursor.style.userSelect = 'none';
+
+  // Add the image to the document body
+  document.body.appendChild(customCursor);
+
+  // Update image position to follow mouse
+  document.addEventListener('mousemove', (event) => {
+    // Position custom cursor centered on mouse pointer
+    customCursor.style.left = (event.clientX - customCursor.width / 2) + 'px';
+    customCursor.style.top = (event.clientY - customCursor.height / 2) + 'px';
+  });
+}
+
+function collisionDetected() {
+    if(bees[0].x >= mouseX-10 &&
+      bees[0].y >= mouseY-10 &&
+      bees[0].x <= mouseX &&
+      bees[0].y <= mouseY ) {
+      collision = true;
+  }
+  if(bees[1].x >= mouseX-10 &&
+      bees[1].y >= mouseY-10 &&
+      bees[1].x <= mouseX &&
+      bees[1].y <= mouseY ) {
+      collision = true;
+  }
+  if(bees[2].x >= mouseX-10 &&
+      bees[2].y >= mouseY-10 &&
+      bees[2].x <= mouseX &&
+      bees[2].y <= mouseY ) {
+      collision = true;
+  }
+  if(bees[3].x >= mouseX-10 &&
+      bees[3].y >= mouseY-10 &&
+      bees[3].x <= mouseX &&
+      bees[3].y <= mouseY ) {
+      collision = true;
+  }
+  if(bees[4].x >= mouseX-10 &&
+      bees[4].y >= mouseY-10 &&
+      bees[4].x <= mouseX &&
+      bees[4].y <= mouseY ) {
+      collision = true;
+  }
 }
 
 beeimg.onload = () => {
+  isAnimating = false;
   draw();
 };
 
-myCanvas.addEventListener("mousemove", recordMousePos);
-
+myCanvas.addEventListener('mousemove', recordMousePos);
+myCanvas.addEventListener('mouseleave', beeStop);
+myCanvas.addEventListener('mouseenter', beeStart);
