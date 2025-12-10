6953ddd853fec84011e80944e8016c473a8e521b
DylanWeiner <adhdylanaza@gmail.com>
2025-12-10 11:41:30 -0700

    Correcting HW 3

--- CS6012/Day 4/SortedArraySet/src/assignment03/BinarySearchSet.java
+++ CS6012/Day 4/SortedArraySet/src/assignment03/BinarySearchSet.java
@@ -171,7 +171,7 @@ public class BinarySearchSet<E> implements SortedSet<E> {
 //                    throw new IllegalStateException();
 //                }
                 if(canRemove) {
-                    BinarySearchSet.this.remove(set[size - 1]);
+                    BinarySearchSet.this.remove(set[--index]);
                     canRemove = false;
                     size--;
                 }
