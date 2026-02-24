1fb312393d54d72938afeacfabd94ee77219c3aa
DylanWeiner <adhdylanaza@gmail.com>
2026-02-24 11:09:20 -0700

    Fixed _let parser

--- CS6015/Project Phase 6/main.cpp
+++ CS6015/Project Phase 6/main.cpp
@@ -9,13 +9,6 @@
 // #endif
 
 int main(int argc, char **argv) {
-  // Expr *e = parse_str("1+2+3+4"); /// Parses user input.
-
-  // std::cout << "Expression: ";
-  // e->printExp(std::cout);
-  // std::cout << "\n";
-  // std::cout << "Value: " << e->interp() << "\n";
-
   int action = use_arguments(argc, argv); /// Reads arguments from the commandline into the use_arguments method.
 
   if(action == do_interp) {
