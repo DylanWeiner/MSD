4d5884abc0dad170ef3232f80e74082c3cc967df
DylanWeiner <adhdylanaza@gmail.com>
2026-04-01 12:52:44 -0600

    Updated HashTable

--- CS6013/Malloc/MyMalloc.cpp
+++ CS6013/Malloc/MyMalloc.cpp
@@ -3,11 +3,13 @@
 // Assignment: Malloc Replacement
 
 #include "MyMalloc.h"
+#include <unistd.h>
 
 void* MyMalloc::allocate(size_t bytesToAllocate) {
-    void* ptr = mmap(nullptr, bytesToAllocate, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0); // Allocate memory using mmap
-    allocatedBlocks.insert(ptr, bytesToAllocate);
-    // size_t pageSize = static_cast<size_t>(getpagesize());
+    size_t pageSize = static_cast<size_t>(getpagesize());
+    size_t roundedSize = ((bytesToAllocate + pageSize -1) / pageSize) * pageSize;
+    void* ptr = mmap(nullptr, roundedSize, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0); // Allocate memory using mmap
+    allocatedBlocks.insert(ptr, roundedSize);
     return ptr;
 }
 
--- CS6013/Malloc/hashTable.cpp
+++ CS6013/Malloc/hashTable.cpp
@@ -4,9 +4,9 @@
 
 hashTable::hashTable() { 
     this->capacity = 100; // Initial capacity
-    this->numBytes = sizeof(hTable); // Initial number of bytes allocated
+    this->numBytes = sizeof(HashEntry) * capacity; // Initial number of bytes allocated
     
-    addressTable = static_cast<hTable*>(mmap(this, numBytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0)); // Allocate memory for the hash table
+    addressTable = static_cast<HashEntry*>(mmap(this, numBytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0)); // Allocate memory for the hash table
 
     if (addressTable == MAP_FAILED) {
         addressTable = nullptr;
@@ -15,20 +15,20 @@ hashTable::hashTable() {
         return;
     } // Catches in case of memory mapping failure
 
-    addressTable->size = 0; // Initial size
-    addressTable->capacity = capacity;
+    size = 0; // Initial size
 
-    for(int i = 0; i < addressTable->capacity; i++) {
-        addressTable->address[i] = nullptr;
-        addressTable->allocationSize[i] = 0;
+    for(size_t i = 0; i < capacity; i++) {
+        addressTable[i].address = nullptr;
+        addressTable[i].allocationSize = 0;
+        addressTable[i].state = 0;
     }
 }
 
 // hashTable::hashTable(int capacity) { 
-//     this->capacity = 100; // Initial capacity
-//     this->numBytes = sizeof(hTable); // Initial number of bytes allocated
+//     this->capacity = capacity; // Initial capacity
+//     this->numBytes = sizeof(HashEntry); // Initial number of bytes allocated
     
-//     addressTable = static_cast<hTable*>(mmap(this, numBytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0)); // Allocate memory for the hash table
+//     addressTable = static_cast<HashEntry*>(mmap(this, numBytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0)); // Allocate memory for the hash table
 
 //     if (addressTable == MAP_FAILED) {
 //         addressTable = nullptr;
@@ -46,6 +46,32 @@ hashTable::hashTable() {
 //     }
 // }
 
+int hashTable::find(void* desiredAddress) {
+    for(size_t i = 0; i < capacity; i++) {
+        if (addressTable[i].address == desiredAddress && addressTable[i].state == 1) {
+            int* ptr = (int*)addressTable[i].address;
+            return *ptr;
+        } //This might be wrong
+    }
+    return -1; // Address not found
+}
+
+void hashTable::grow() {
+    size_t oldCapacity = capacity;
+    size_t oldBytes = numBytes;
+    HashEntry* oldTable = addressTable;
+    capacity = oldCapacity * 2;
+    numBytes = oldBytes * capacity; // Initial number of bytes allocated
+    addressTable = static_cast<HashEntry*>(mmap(this, numBytes, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0)); // Allocate memory for the hash table
+
+    for(size_t i = 0; i < oldCapacity; i++) {
+        insert(oldTable[i].address, oldTable[i].allocationSize);
+    }
+    munmap(oldTable, oldBytes);
+}
+
+
+
 hashTable::~hashTable() { 
     if (addressTable != nullptr) {
         munmap(addressTable, numBytes);
@@ -57,14 +83,15 @@ hashTable::~hashTable() {
 }
 
 void hashTable::insert(void* address, size_t sizeOfAllocation) {
-    if(this->addressTable->size >= addressTable->capacity-1) {
-        *addressTable = this->addressTable->grow(); // Grow the hash table if the size exceeds the capacity
+    if(this->size >= capacity - 1) {
+        this->grow(); // Grow the hash table if the size exceeds the capacity
     }
     for(int i = 0; i < this->capacity; i++) {
-        if (this->addressTable->address[i] == nullptr) {
-            this->addressTable->allocationSize[i] = sizeOfAllocation;
-            this->addressTable->address[i] = address; // Insert the address into the hash table
-            this->addressTable->size++; // Increment the size of the hash table
+        if (addressTable[i].address == nullptr) {
+            addressTable[i].allocationSize = sizeOfAllocation;
+            addressTable[i].address = address; // Insert the address into the hash table
+            addressTable[i].state = 1;
+            size++; // Increment the size of the hash table
             break;
         }
     }
@@ -73,24 +100,25 @@ void hashTable::insert(void* address, size_t sizeOfAllocation) {
 size_t hashTable::remove(void* address) {
     size_t sizeOfAllocation;
     for(int i = 0; i < this->capacity; i++) {
-        if (this->addressTable->address[i] == address) {
-            this->addressTable->address[i] = nullptr; // Remove the address from the hash table
-            sizeOfAllocation = addressTable->allocationSize[i];
+        if (addressTable[i].address == address) {
+            addressTable[i].address = nullptr; // Remove the address from the hash table
+            sizeOfAllocation = addressTable[i].allocationSize;
+            addressTable[i].state = 0;
             break;
         }
     }
-    this->addressTable->size--; // Decrement the size of the hash table
+    this->size--; // Decrement the size of the hash table
     return sizeOfAllocation;
 }
 
 void* hashTable::getAddress(int location) {
-    return this->addressTable->address[location];
+    return addressTable[location].address;
 }
 
 size_t hashTable::getSize() {
-    return this->addressTable->size;
+    return this->size;
 }
 
 size_t hashTable::getCapacity() {
-    return this->addressTable->capacity;
+    return this->capacity;
 }
