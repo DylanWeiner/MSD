21ef0b5cac5c72b2fcb6aa78a471ae96e2e0b8e3
DylanWeiner <adhdylanaza@gmail.com>
2026-04-22 09:51:45 -0600

    Fixed errors described in assignment comments

--- CS6013/Lab 3/lab3.cpp
+++ CS6013/Lab 3/lab3.cpp
@@ -1,30 +1,57 @@
+// By Dylan Weiner
+// This program demonstrates inter-process communication using pipes and signals in C++.
+// 2/3/26
+// CS 6013
+
 #include <iostream>
 #include <unistd.h>
+#include <signal.h>
 
 using namespace std;
+
+volatile bool childAcknowledged = false;
+
+void signalHandler(int sig) {
+    if(sig == SIGUSR1) {
+        childAcknowledged = true;
+    }
+}
 
 void usage(char * argv[]) {
     cout << argv[0] <<": 'message'\n";
 }
 
-void childCode(int readFd) {
+void childCode(int readFd, pid_t parentPid) {
     cout << "Child:\n";
 
-    // Get length
-    int length;
-    read(readFd, &length, 4);
-    cout << "Child thinks message has a length of " << length << "\n";
+    bool done = false;
+    while(!done) {
+        // Get length
+        int length;
+        read(readFd, &length, 4);
+        
+        if(length == -1) { // Special signal to quit
+            done = true;
+            cout << "Child: Received quit signal\n";
+            break;
+        }
+        
+        cout << "Child thinks message has a length of " << length << "\n";
 
-    // Get actual message.
-    char msg[1024]; // Creates a buffer up to 1024 bytes.
-    read(readFd, msg, length);
+        // Get actual message.
+        char msg[1024]; // Creates a buffer up to 1024 bytes.
+        read(readFd, msg, length);
 
-    cout << "Child: " << msg << "\n";
+        cout << "Child: " << msg << "\n";
+        
+        // Send acknowledgement signal to parent
+        kill(parentPid, SIGUSR1);
+    }
     
     exit(0);
 }
 
-void parentCode(int writeFd, const string & message) { // We want to wait for the child code to finish.
+void parentCode(int writeFd, const string & message, pid_t childPid) { // We want to wait for the child code to finish.
     cout << "Parent:\n";
 
     int len = message.length() + 1;
@@ -38,6 +65,24 @@ void parentCode(int writeFd, const string & message) { // We want to wait for th
         string newMsg;
         getline(cin, newMsg);
         
+        if(newMsg == "quit") {
+            // Send quit signal to child
+            int quitSignal = -1;
+            write(writeFd, &quitSignal, 4);
+            done = true;
+        } else {
+            // Write the new message to child
+            int len = newMsg.length() + 1;
+            write(writeFd, &len, 4);
+            write(writeFd, newMsg.c_str(), newMsg.length() + 1);
+            
+            // Wait for acknowledgement from child
+            childAcknowledged = false;
+            while(!childAcknowledged) {
+                pause(); // Wait for signal
+            }
+            cout << "Parent: Child acknowledged.\n";
+        }
     }
 
     int status;
@@ -70,11 +115,14 @@ int main(int argc, char * argv[]) {
         exit(1);
     }
     else if(rc == 0) { // 0 Marks the child process.
-        childCode(readFd);
+        childCode(readFd, getppid());
     }
     else { // This is the parent process.
+        // Set up signal handler for acknowledgements
+        signal(SIGUSR1, signalHandler);
+        
         string msg = argv[1];
-        parentCode(writeFd, msg);
+        parentCode(writeFd, msg, rc);
     }
 }
 
